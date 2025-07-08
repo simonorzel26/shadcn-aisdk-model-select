ea7784b5eb8bd54dbfbd2095d92ff23ce90af102
Simon Orzel <simonorzel@gmail.com>
2025-07-09 01:31:02 +0200

    wip

--- src/components/model-select-package/components/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './ModelSettingsDialog';
-export * from './ModelSelectDropdown';
-export * from './ModelSelectionTab';
-export * from './TinyModelSelector';
-export { useApiKeys } from '../hooks/useApiKeys';
-export { ModelSelectionProvider, useModelSelection } from '../contexts/ModelSelectionContext';
